823f1885138cf508637d2c1cba21ca61154abbc3
GilbertusKristo <112887683+GilbertusKristo@users.noreply.github.com>
2025-05-01 13:25:10 +0700

    new db name

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -6,7 +6,7 @@ import { DATABASE_URL } from "./env";
 const connect = async () => {
     try {
         await mongoose.connect(DATABASE_URL, {
-            dbName: "db-event",
+            dbName: "db-absensi",
         });
 
         return Promise.resolve("Database connected successfully");
